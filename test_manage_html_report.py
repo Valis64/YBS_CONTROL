090b08f11e4fb5137e3b24f7d93dae8a89716360
Valis64 <neone7one@gmail.com>
2025-08-13 15:00:49 -0700

    Add realtime report writer and tests

--- test_manage_html_report.py
+++ test_manage_html_report.py
@@ -4,12 +4,16 @@ import unittest
 from datetime import datetime
 import sys
 import argparse
+import csv
+
+from bs4 import BeautifulSoup
 
 import manage_html_report
 from manage_html_report import (
     compute_lead_times,
     parse_manage_html,
     generate_realtime_report,
+    write_realtime_report,
 )
 
 SAMPLE_HTML = """
@@ -39,6 +43,33 @@ SAMPLE_HTML_TEMPLATE = """
 <ul class="workplaces">
 <li><p><span class="circle"></span>Step1</p><p class="np">07/22/25 10:00</p></li>
 <li class="active_ws"><p><span class="circle"></span>Step2</p><p class="np">&nbsp;</p></li>
+</ul>
+</td>
+</tr>
+</tbody>
+"""
+
+SAMPLE_HTML_MULTI = """
+<tbody id="table">
+<tr data-id="1">
+<td class="move"><p>YBS 1001</p></td>
+<td></td>
+<td></td>
+<td>
+<ul class="workplaces">
+<li><p><span class="circle"></span>Prep</p><p class="np">07/22/25 10:00</p></li>
+<li><p><span class="circle"></span>Print</p><p class="np">07/22/25 11:00</p></li>
+</ul>
+</td>
+</tr>
+<tr data-id="2">
+<td class="move"><p>YBS 1002</p></td>
+<td></td>
+<td></td>
+<td>
+<ul class="workplaces">
+<li><p><span class="circle"></span>Prep</p><p class="np">07/21/25 09:00</p></li>
+<li><p><span class="circle"></span>Print</p><p class="np">07/21/25 10:00</p></li>
 </ul>
 </td>
 </tr>
@@ -111,6 +142,50 @@ class ManageHTMLTests(unittest.TestCase):
         self.assertEqual(start, datetime(2025, 7, 22, 10, 0))
         self.assertEqual(end, datetime(2025, 7, 23, 15, 0))
         self.assertAlmostEqual(hours, 13.5)
+
+    def test_write_realtime_report(self):
+        with tempfile.NamedTemporaryFile("w+", delete=False, suffix=".html") as tmp:
+            tmp.write(SAMPLE_HTML_MULTI)
+            html_path = tmp.name
+
+        jobs = parse_manage_html(html_path)
+        report = generate_realtime_report(jobs)
+        csv_fd, csv_path = tempfile.mkstemp(suffix=".csv")
+        html_fd, out_html_path = tempfile.mkstemp(suffix=".html")
+        os.close(csv_fd)
+        os.close(html_fd)
+
+        try:
+            write_realtime_report(report, csv_path, out_html_path)
+
+            # verify CSV output
+            with open(csv_path, newline="") as f:
+                rows = list(csv.reader(f))
+            self.assertEqual(
+                rows[0],
+                ["job_number", "workstation", "start", "end", "hours_in_queue"],
+            )
+            # chronological order (job 1002 starts earlier than 1001)
+            self.assertEqual(rows[1][0], "1002")
+            self.assertEqual(rows[2][0], "1001")
+            # date formatting
+            datetime.strptime(rows[1][2], manage_html_report.HTML_DATE_FORMAT)
+            datetime.strptime(rows[1][3], manage_html_report.HTML_DATE_FORMAT)
+
+            # verify HTML output
+            with open(out_html_path, encoding="utf-8") as f:
+                soup = BeautifulSoup(f, "html.parser")
+            headers = [th.get_text() for th in soup.select("thead tr th")]
+            self.assertEqual(
+                headers,
+                ["job_number", "workstation", "start", "end", "hours_in_queue"],
+            )
+            first_row = [td.get_text() for td in soup.select("tbody tr")[0].find_all("td")]
+            self.assertEqual(first_row[0], "1002")
+        finally:
+            os.remove(html_path)
+            os.remove(csv_path)
+            os.remove(out_html_path)
 
     def test_compute_lead_times_date_range(self):
         jobs = parse_manage_html(self.tmp_path)
